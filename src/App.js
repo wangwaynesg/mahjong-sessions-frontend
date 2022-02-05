d7e42dc6ea81d762c781757659297f0901e97092
Wang Wayne <wangwaynesg@gmail.com>
2022-02-05 17:27:23 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>This is my mahjong-sessions app</h1>
+        </div>
+    );
+}
+
+export default App;
